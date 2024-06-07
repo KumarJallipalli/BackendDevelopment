37e48dfe17805d99ad2a84523ae0dd855665e0ab
Siva Jallipalli <kumar.jallipalli@gmail.com>
2024-06-07 22:55:07 +0530

    First Express App

---
+++ 02. Deployment/index.js
@@ -0,0 +1,19 @@
+// importing the express module/library using require method
+const express = require('express');
+
+// express() →  creates an Express Application & returns an Object
+const app = express();
+
+// ports are logical endpoints used to exchange information between a web server & a web client 
+const port = 3000;
+
+// It handles the GET request on the specified path by executing the callback fn
+app.get('/', (req, res) => {
+  // string data is sent as a response   
+  res.send('Hello World!');
+});
+
+// app.listen() → listens to all the incoming connections/requests on specified port 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
